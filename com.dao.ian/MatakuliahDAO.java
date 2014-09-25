e4d1bc5a3e9d8c857e917ce072f8905eb0329504
Ibnu Aulia Nugraha <ibnuauliana@gmail.com>
2014-09-24 17:26:38 -0700

    Create MatakuliahDAO.java

---
+++ com.dao.ian/MatakuliahDAO.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pertemuan.dao.ian;
+
+import java.util.List;
+import pertemuan.model.ian.ClassMatakuliah;
+
+/**
+ *
+ * @author Ian-Nano
+ */
+public interface MatakuliahDAO {
+    public void insert(ClassMatakuliah classMatakuliah);
+    public void delete(String kode_matakuliah);
+    public void update(String oldKode_matakuliah, ClassMatakuliah classMatakuliah);
+    public ClassMatakuliah loadNama(String nama_matakuliah);
+    public ClassMatakuliah load(String kode_matakuliah);
+    public List<ClassMatakuliah>getAll();
+    public List<ClassMatakuliah>getAll(String nama_matakuliah);
+    public List<ClassMatakuliah>getAllNim (String kode_matakuliah);
+}
