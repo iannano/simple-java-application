017a866481cfc7b45358acb06fd383875acf54b9
Ibnu Aulia Nugraha <ibnuauliana@gmail.com>
2014-09-24 17:56:14 -0700

    Create ClassMahasiswa.java

---
+++ com.model.ian/ClassMahasiswa.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.model.ian;
+
+/**
+ *
+ * @author Ian-Nano
+ */
+public class ClassMahasiswa {
+    private String nim;
+    private String nama;
+    private String alamat;
+    private String telphone;
+
+    public String getNim() {
+        return nim;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public String getTelphone() {
+        return telphone;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public void setTelphone(String telphone) {
+        this.telphone = telphone;
+    }
+}
