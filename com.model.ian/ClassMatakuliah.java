80f5ad0d096d9be6440e0402c594a3e373a6529d
Ibnu Aulia Nugraha <ibnuauliana@gmail.com>
2014-09-24 17:56:40 -0700

    Create ClassMatakuliah.java

---
+++ com.model.ian/ClassMatakuliah.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.model.ian;
+
+/**
+ *
+ * @author Ian-Nano
+ */
+public class ClassMatakuliah {
+    private String kode_matakuliah;
+    private String nama_matakuliah;
+    private int sks_matakuliah;
+
+    public String getKode_matakuliah() {
+        return kode_matakuliah;
+    }
+
+    public String getNama_matakuliah() {
+        return nama_matakuliah;
+    }
+
+    public int getSks_matakuliah() {
+        return sks_matakuliah;
+    }
+
+    public void setKode_matakuliah(String kode_matakuliah) {
+        this.kode_matakuliah = kode_matakuliah;
+    }
+
+    public void setNama_matakuliah(String nama_matakuliah) {
+        this.nama_matakuliah = nama_matakuliah;
+    }
+
+    public void setSks_matakuliah(int sks_matakuliah) {
+        this.sks_matakuliah = sks_matakuliah;
+    }
+    
+    
+}
